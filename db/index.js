bf3018689ae1a9ce76a85f89691bd4f41cd7c218
Dalron J Robertson <112004061+DJRobertson95@users.noreply.github.com>
2022-11-10 16:55:28 -0600

    updates for tori and me

--- db/index.js
+++ db/index.js
@@ -1,13 +1,22 @@
+require('dotenv').config ();
+
 // This is where we will be building our pg.Client instance;
 const pg = require('pg');
-const client = new pg.Client({
-    host: 'localhost',
-    database: 'fitness-dev',
-    port: 5432,
-    //need your usernmae and password
-    user: ,
-    password: ,
-});
+let client
+
+if (process.env.user && process.env.password ) {
+    client = new pg.Client({
+        host: 'localhost',
+        database: 'fitness-dev',
+        port: 5432,
+        //need your usernmae and password
+        user: process.env.user,
+        password: process.env.password,
+    });
+} else {
+    client = new pg.Client('postgres://localhost:5432/fitness-dev');
+}
+
 
 module.exports = {
     client
--- index.js
+++ index.js
@@ -2,7 +2,12 @@
 
 const express = require('express');
 const {client} = require('./db/index');
+const {usersRouter} = require('./routes/users');
+
+// Setting up a new express server instance
 const app = express();
+
+// Sub Router
 
 
 app.listen(3000, () => {
