49ce376431b53d48b272fbad0c87fc1430df1f8f
Tori Deblinger <vdeblinger@yahoo.com>
2022-11-10 16:33:09 -0600

    Second commit

--- db/index.js
+++ db/index.js
@@ -1,1 +1,14 @@
 // This is where we will be building our pg.Client instance;
+const pg = require('pg');
+const client = new pg.Client({
+    host: 'localhost',
+    database: 'fitness-dev',
+    port: 5432,
+    //need your usernmae and password
+    user: ,
+    password: ,
+});
+
+module.exports = {
+    client
+}
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // This is where we will be building our Express server;
+
+const express = require('express');
+const {client} = require('./db/index');
+const app = express();
+
+
+app.listen(3000, () => {
+    console.log('We are now running on port 3000')
+});
